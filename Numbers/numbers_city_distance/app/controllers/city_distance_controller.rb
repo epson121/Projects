bbe5ab92fb3a567513e75681bcfa2ad1ca3b345b
epson121 <traxdater12@gmail.com>
2013-08-15 00:38:21 +0200

    update for city distance

--- Numbers/numbers_city_distance/app/controllers/city_distance_controller.rb
+++ Numbers/numbers_city_distance/app/controllers/city_distance_controller.rb
@@ -4,12 +4,12 @@ class CityDistanceController < ApplicationController
   
   def index
   	#add regex checking
-  	#not suitable for close distances (lat lng issue??)
   	if !params[:pl].nil? and !params[:pr].nil? and !params[:pl].empty? and !params[:pr].empty?
 	  		@left = Geocoder.search(params[:pl])
 	  		@right = Geocoder.search(params[:pr])
-	  		@distance = "%.6f km" % Geocoder::Calculations.distance_between(	[@left[0].latitude, @left[0].longitude], 
-	  																																 	[@right[0].latitude, @right[0].longitude]) 
+	  		@distance = Geocoder::Calculations.distance_between([@left[0].latitude, @left[0].longitude], 
+	  																																 	[@right[0].latitude, @right[0].longitude]) * 1.60934
+	  		@distance = "%.2f km" % @distance
 	  		respond_to do |format|
 	  			format.js
 				end
